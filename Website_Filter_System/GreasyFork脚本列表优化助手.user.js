911a50239951e59583e0ba835077ebd6b716fe4c
浴火凤凰 <phoenix_in_the_sky@foxmail.com>
2019-12-08 23:39:03 +0800

    添加过滤搜索框

--- Website_Filter_System/GreasyFork脚本列表优化助手.user.js
+++ Website_Filter_System/GreasyFork脚本列表优化助手.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/kingphoenix2000/tampermonkey_scripts
 // @supportURL   https://github.com/kingphoenix2000/tampermonkey_scripts
 // @downloadURL  https://github.com/kingphoenix2000/tampermonkey_scripts/raw/master/Website_Filter_System/GreasyFork%E8%84%9A%E6%9C%AC%E5%88%97%E8%A1%A8%E4%BC%98%E5%8C%96%E5%8A%A9%E6%89%8B.user.js
-// @version      0.1.0
+// @version      0.1.1
 // @author       浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
 // @description  此脚本会在GreasyFork网站的脚本列表页面每个脚本的下面添加几个快捷操作的按钮。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
 // @homepage     https://blog.csdn.net/kingwolf_javascript/
