1e5d53d656ad42cdf87b4ae08d945e1667b03d43
浴火凤凰 <phoenix_in_the_sky@foxmail.com>
2020-02-22 21:47:39 +0800

    增加脚本使用和提示说明信息，增加 显示更多电影 按钮

--- Website_Filter_System/电影列表排序助手.user.js
+++ Website_Filter_System/电影列表排序助手.user.js
@@ -3,12 +3,13 @@
 // @namespace    https://github.com/kingphoenix2000/tampermonkey_scripts
 // @supportURL   https://github.com/kingphoenix2000/tampermonkey_scripts
 // @downloadURL  https://github.com/kingphoenix2000/tampermonkey_scripts/raw/master/Website_Filter_System/%E7%94%B5%E5%BD%B1%E5%88%97%E8%A1%A8%E6%8E%92%E5%BA%8F%E5%8A%A9%E6%89%8B.user.js
-// @version      0.1.0
+// @version      0.1.1
 // @author       浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
 // @description  给包含很长电影列表的单个页面添加几个筛选排序功能，比如按照年份、评分等对电影列表重新进行排序。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
 // @homepage     https://blog.csdn.net/kingwolf_javascript/
 // @include      https://leisidianying.com/*
 // @grant        GM_openInTab
+// @note         2020-02-22：1.增加使用说明
 // ==/UserScript==
 
 
@@ -24,15 +25,53 @@
 
     function setButton() {
         let div = document.createElement("div");
+        div.id = "sortHelper";
         let h4 = document.createElement("h4");
-        h4.innerHTML = '<a href="https://greasyfork.org/zh-CN/users/289205-%E6%B5%B4%E7%81%AB%E5%87%A4%E5%87%B0" target="_blank">电影列表排序助手，作者：浴火凤凰(QQ:307053741)</a>';
+        let h5 = document.createElement("h5");
+        h4.innerHTML = '<a href="https://greasyfork.org/zh-CN/users/289205-%E6%B5%B4%E7%81%AB%E5%87%A4%E5%87%B0" target="_blank">★★★电影列表排序助手，作者：浴火凤凰(QQ:307053741)★★★</a>';
+        let totalMovie = document.querySelector(".block-main.block-main-first div.item-name div.rat span[title=rating]").innerText;
+        let loaded = document.querySelectorAll("#tag-movie-list > div.item").length;
+        let title = document.querySelector("#block-tag-movie-list > div.heading-c > h2");
+        title.innerHTML = title.innerText + '<font color="red">（30/100）</font>'.replace("30", loaded).replace("100", totalMovie);
+        let info = '<div style="color:red;">1.本标签系列共有电影' + totalMovie + '部，首次打开页面只显示了' + loaded + '部，想要显示更多电影请点击【显示更多电影】按钮。</div>';
+        info += '<div style="color:red;">2.点击【显示更多电影】按钮之后，需要再次点击筛选按钮，才会对新加载的电影进行排序。</div>';
+        h5.innerHTML = info;
+        let input0 = document.createElement("input");
         let input1 = document.createElement("input");
         let input2 = document.createElement("input");
+        input0.type = "button";
+        input0.value = "显示更多电影";
         input1.type = "button";
         input1.value = "年份从远到近";
+        input1.style.marginLeft = "15px";
         input2.type = "button";
         input2.value = "年份从近到远";
         input2.style.marginLeft = "15px";
+        input0.onclick = function () {
+            var iRelListNextPage = iTagMovieListCurrentPage + 1;
+            $('#buttonMoreTagMovieListContainer button').attr('disabled', 'disabled');
+            document.querySelector("#sortHelper input:first-of-type").value = "正在加载...";
+            document.querySelector("#sortHelper input:first-of-type").disabled = true;
+            $.ajax({
+                url: sTagMovieListMoreUrl + '&order=' + iTagMovieListOrder + '&page=' + iRelListNextPage,
+                success: function (data) {
+                    $('#tag-movie-list').append(data);
+                    iTagMovieListCurrentPage++;
+                    let loaded = document.querySelectorAll("#tag-movie-list > div.item").length;
+                    let title = document.querySelector("#block-tag-movie-list > div.heading-c > h2 > font");
+                    title.innerText = '（' + loaded + '/' + totalMovie + '）';
+                    if (iTagMovieListCurrentPage == iTagMovieListLastPage) {
+                        $('#buttonMoreTagMovieListContainer').remove();
+                        document.querySelector("#sortHelper input:first-of-type").value = "全部加载完毕";
+                        document.querySelector("#sortHelper input:first-of-type").disabled = true;
+                    } else {
+                        $('#buttonMoreTagMovieListContainer button').removeAttr('disabled');
+                        document.querySelector("#sortHelper input:first-of-type").value = "显示更多电影";
+                        document.querySelector("#sortHelper input:first-of-type").disabled = false;
+                    }
+                }
+            })
+        }
         input1.onclick = input2.onclick = function () {
             let items = document.querySelectorAll("#tag-movie-list > div.item");
             let len = items.length;
@@ -97,6 +136,8 @@
         }
 
         div.appendChild(h4);
+        div.appendChild(h5);
+        div.appendChild(input0);
         div.appendChild(input1);
         div.appendChild(input2);
         div.appendChild(input3);
