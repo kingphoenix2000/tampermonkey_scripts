94322c41a6820346063ad23102413736b905d5ea
浴火凤凰 <phoenix_in_the_sky@foxmail.com>
2020-04-11 12:03:48 +0800

    修改描述

--- Page Automation/删除链接重定向.user.js
+++ Page Automation/删除链接重定向.user.js
@@ -8,9 +8,9 @@
 // @updateURL             https://github.com/kingphoenix2000/tampermonkey_scripts/raw/master/Page%20Automation/%E5%88%A0%E9%99%A4%E9%93%BE%E6%8E%A5%E9%87%8D%E5%AE%9A%E5%90%91.user.js
 // @version               0.1.2
 // @author                浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
-// @description           本脚本提供了一系列小工具，在您访问互联网网站的时候加快您访问网站的速度和提高您的工作效率。
+// @description           本脚本提供了一系列小工具，在您访问互联网网站的时候加快您访问网站的速度和提高您的工作效率。包括删除服务器重定向(支持全网网站)
 // @description:en        This script provides a series of small tools to speed up the speed of your visit to the website and improve your work efficiency.
-// @description:zh-CN     本脚本提供了一系列小工具，在您访问互联网网站的时候加快您访问网站的速度和提高您的工作效率。
+// @description:zh-CN     本脚本提供了一系列小工具，在您访问互联网网站的时候加快您访问网站的速度和提高您的工作效率。包括删除服务器重定向(支持全网网站)
 // @homepage              https://blog.csdn.net/kingwolf_javascript/
 // @include               *
 // @grant                 GM_getValue
