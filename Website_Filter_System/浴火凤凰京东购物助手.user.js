6148676d73aeb349eae61bbf1325ca3b34fce68f
浴火凤凰 <phoenix_in_the_sky@foxmail.com>
2020-04-12 14:58:39 +0800

    增加根据关键字过滤功能

--- Website_Filter_System/浴火凤凰京东购物助手.user.js
+++ Website_Filter_System/浴火凤凰京东购物助手.user.js
@@ -8,9 +8,9 @@
 // @updateURL             https://github.com/kingphoenix2000/tampermonkey_scripts/raw/master/Website_Filter_System/%E6%B5%B4%E7%81%AB%E5%87%A4%E5%87%B0%E4%BA%AC%E4%B8%9C%E8%B4%AD%E7%89%A9%E5%8A%A9%E6%89%8B.user.js
 // @version               0.1.1
 // @author                浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
-// @description           在京东商城的商品搜索结果页面顶部生成一个筛选按钮，点击按钮会生成一些筛选条件，可以按照商品店铺、商品包含的内容关键字、商品评价数、商品的标签等等类别进行筛选。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
-// @description:en        在京东商城的商品搜索结果页面顶部生成一个筛选按钮，点击按钮会生成一些筛选条件，可以按照商品店铺、商品包含的内容关键字、商品评价数、商品的标签等等类别进行筛选。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
-// @description:zh-CN     在京东商城的商品搜索结果页面顶部生成一个筛选按钮，点击按钮会生成一些筛选条件，可以按照商品店铺、商品包含的内容关键字、商品评价数、商品的标签等等类别进行筛选。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
+// @description           在京东商城的商品搜索结果页面商品列表顶部生成一个筛选按钮，点击按钮会生成一系列筛选条件，可以按照店铺、商品标签、商品内容包含的关键字、商品价格、商品评价数等等类别进行筛选。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
+// @description:en        Generate a filter button at the top of the goods list on the goods search results page of JD.com. Clicking the button will generate a series of filter conditions, so goods can be filtered according to the categories of shop name, goods label, keywords, goods price, goods comments, etc.作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
+// @description:zh-CN     在京东商城的商品搜索结果页面商品列表顶部生成一个筛选按钮，点击按钮会生成一系列筛选条件，可以按照店铺、商品标签、商品内容包含的关键字、商品价格、商品评价数等等类别进行筛选。作者：浴火凤凰(QQ:307053741,油猴脚本讨论QQ群:194885662)
 // @homepage              https://blog.csdn.net/kingwolf_javascript/
 // @include               https://search.jd.com/*
 // @grant                 none
@@ -84,7 +84,7 @@
         function setTypeSelection(object) {
             let select = document.createElement("select");
             select.innerHTML = '';
-            select.style.cssText = "width: 180px;height: 23px;";
+            select.style.cssText = "width: 250px;height: 23px;";
             for (const key in object) {
                 if (object.hasOwnProperty(key)) {
                     const value = object[key];
@@ -136,6 +136,12 @@
                     case "comment":
                         handleCommentFilter("list-item", "none");
                         break;
+                    case "allkeyword":
+                        handleAllkeywordFilter("list-item", "none");
+                        break;
+                    case "anykeyword":
+                        handleAnykeywordFilter("list-item", "none");
+                        break;
                     default:
                         break;
                 }
@@ -160,6 +166,12 @@
                         break;
                     case "comment":
                         handleCommentFilter("none", "list-item");
+                        break;
+                    case "allkeyword":
+                        handleAllkeywordFilter("none", "list-item");
+                        break;
+                    case "anykeyword":
+                        handleAnykeywordFilter("none", "list-item");
                         break;
                     default:
                         break;
@@ -226,6 +238,28 @@
             let div2 = document.createElement("div");
             div2.innerHTML = '<input type="text" placeholder="请输入评价数" value="" style="width:100px;height:23px;" />万+条评价';
             div2.id = 'comment_div';
+            div2.style.cssText = "display:none;"
+            div2.insertBefore(div1, div2.firstChild);
+            addDiv(div2);
+        }
+        function setAllkeywordFilter() {
+            let div1 = document.createElement("h3");
+            div1.innerHTML = "筛选规则：最多支持输入5个关键字，以空格隔开。商品必须同时包含输入的所有关键字才算符合筛选结果，单个关键字不受影响。";
+            div1.style.cssText = "margin:5px;color:red;";
+            let div2 = document.createElement("div");
+            div2.innerHTML = '<input type="text" placeholder="请输入多个关键字，以空格隔开" value="" style="width:300px;height:23px;" />';
+            div2.id = 'allkeyword_div';
+            div2.style.cssText = "display:none;"
+            div2.insertBefore(div1, div2.firstChild);
+            addDiv(div2);
+        }
+        function setAnykeywordFilter() {
+            let div1 = document.createElement("h3");
+            div1.innerHTML = "筛选规则：最多支持输入5个关键字，以空格隔开。商品包含输入的任何一个关键字就算符合筛选结果，单个关键字不受影响。";
+            div1.style.cssText = "margin:5px;color:red;";
+            let div2 = document.createElement("div");
+            div2.innerHTML = '<input type="text" placeholder="请输入多个关键字，以空格隔开" value="" style="width:300px;height:23px;" />';
+            div2.id = 'anykeyword_div';
             div2.style.cssText = "display:none;"
             div2.insertBefore(div1, div2.firstChild);
             addDiv(div2);
@@ -351,6 +385,72 @@
                 li.style.display = display2;
             }
         }
+        function handleAllkeywordFilter(display1, display2) {
+            let text = document.querySelector("#allkeyword_div > input").value.toLowerCase().trim();
+            let keywords = text.split(/\s+/);
+            let items = document.querySelectorAll("#J_goodsList > ul > li.gl-item");
+            let len = items.length;
+            let validNodes = [];
+            let invalidNodes = [];
+            for (let i = 0; i < len; i++) {
+                let li = items[i];
+                let priceNode = li.querySelector("div.p-name");
+                //判断店铺名字是否相等
+                if (priceNode) {
+                    let text = priceNode.innerText.toLowerCase().trim();
+                    let b = false;//没找到
+                    let len = keywords.length;
+                    if (len > 5) { len = 5; }
+                    for (let i = 0; i < len; i++) {
+                        const keyword = keywords[i];
+                        if (!text.includes(keyword)) { b = true; break; }
+                    }
+                    if (b == true) { invalidNodes.push(li); }
+                    else { validNodes.push(li); }
+                }
+            }
+            for (let i = 0; i < validNodes.length; i++) {
+                const li = validNodes[i];
+                li.style.display = display1;
+            }
+            for (let i = 0; i < invalidNodes.length; i++) {
+                const li = invalidNodes[i];
+                li.style.display = display2;
+            }
+        }
+        function handleAnykeywordFilter(display1, display2) {
+            let text = document.querySelector("#anykeyword_div > input").value.toLowerCase().trim();
+            let keywords = text.split(/\s+/);
+            let items = document.querySelectorAll("#J_goodsList > ul > li.gl-item");
+            let len = items.length;
+            let validNodes = [];
+            let invalidNodes = [];
+            for (let i = 0; i < len; i++) {
+                let li = items[i];
+                let priceNode = li.querySelector("div.p-name");
+                //判断店铺名字是否相等
+                if (priceNode) {
+                    let text = priceNode.innerText.toLowerCase().trim();
+                    let b = false;//没找到
+                    let len = keywords.length;
+                    if (len > 5) { len = 5; }
+                    for (let i = 0; i < len; i++) {
+                        const keyword = keywords[i];
+                        if (text.includes(keyword)) { b = true; break; }
+                    }
+                    if (b == true) { validNodes.push(li); }
+                    else { invalidNodes.push(li); }
+                }
+            }
+            for (let i = 0; i < validNodes.length; i++) {
+                const li = validNodes[i];
+                li.style.display = display1;
+            }
+            for (let i = 0; i < invalidNodes.length; i++) {
+                const li = invalidNodes[i];
+                li.style.display = display2;
+            }
+        }
 
 
 
@@ -358,8 +458,10 @@
         let obj = {
             shopName: "1.按照店铺进行筛选",
             tagName: "2.按照商品标签筛选",
-            price: "3.按照商品价格筛选",
-            comment: "4.按照商品评价数筛选",
+            allkeyword: "3.按照商品同时包含所有关键字筛选",
+            anykeyword: "4.按照商品包含任何一个关键字筛选",
+            price: "5.按照商品价格筛选",
+            comment: "6.按照商品评价数筛选",
         };
         setTypeSelection(obj);
         setShowAllBtn();
@@ -369,6 +471,8 @@
         setTagFilter();
         setPriceFilter();
         setCommentFilter();
+        setAllkeywordFilter();
+        setAnykeywordFilter();
         let lastViewDiv = document.getElementById("shopName_div");
     }
 
@@ -378,14 +482,20 @@
         div.id = "ecommerce_filter_wrapper";
         let ecommerce_filter = document.createElement("div");
         ecommerce_filter.id = "ecommerce_filter";
+        ecommerce_filter.style.cssText = "margin-top: 10px;";
         let button1 = document.createElement("input");
         button1.type = "button";
-        button1.value = "重新生成筛选列表";
+        button1.value = "重新生成筛选系统";
         button1.onclick = function () {
+            let items = document.querySelectorAll("#J_goodsList > ul > li.gl-item");
+            if (items.length < 60) { alert("请先使用鼠标滚动到页面底部\n等待内容全部加载完毕，然后再点击本按钮！"); return; }
             document.querySelector("#ecommerce_filter").innerHTML = "";
             setJDHelper();
         }
+        let span = document.createElement("span");
+        span.innerHTML = '<a style="color:red;font-size: 16px;" href="https://greasyfork.org/zh-CN/users/289205-%E6%B5%B4%E7%81%AB%E5%87%A4%E5%87%B0" target="_blank">★★★京东购物助手，作者：浴火凤凰(QQ:307053741)★★★专业接单Chrome浏览器扩展开发定制、油猴脚本开发定制，欢迎来聊★★★</a>';
         div.appendChild(button1);
+        div.appendChild(span);
         div.appendChild(ecommerce_filter);
         document.querySelector("#J_filter").appendChild(div);
 
